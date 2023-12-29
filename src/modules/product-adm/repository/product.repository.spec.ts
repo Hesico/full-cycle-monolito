4c54bea3981dc4742f868a8ff0711696119bb65c
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2023-12-29 09:22:01 -0300

    Feat: productRepository

--- src/modules/product-adm/repository/product.repository.spec.ts
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import { ProductModel } from "./product.model";
 import Product from "../domain/product.entity";
 import Id from "../../@shared/domain/value-object/id.value-object";
+import ProductRepository from "./product.repository";
 
 describe("Product Repository", () => {
     let sequelize: Sequelize;
@@ -43,10 +44,10 @@ describe("Product Repository", () => {
             id: "1",
             name: "Product 1",
             description: "Product 1 description",
-            purchase_price: 100,
+            purchasePrice: 100,
             stock: 10,
-            created_at: product.createdAt,
-            updated_at: product.updatedAt,
+            createdAt: product.createdAt,
+            updatedAt: product.updatedAt,
         });
     });
 });
