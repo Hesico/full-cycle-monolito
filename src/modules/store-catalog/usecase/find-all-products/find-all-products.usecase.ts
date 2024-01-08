cc8a5884996c81b1854af04bb645f56cb373e787
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2024-01-08 09:42:00 -0300

    feat: product catalog findAll usecase

---
+++ src/modules/store-catalog/usecase/find-all-products/find-all-products.usecase.ts
@@ -0,0 +1,20 @@
+import UsecaseInterface from "../../../@shared/domain/usecase/usecase.interface";
+import ProductGateway from "../../gateway/product.gateway";
+import { FindAllProductsDto } from "./find-all-products.dto";
+
+export default class FindAllProductsUseCase implements UsecaseInterface {
+    constructor(private readonly productRepository: ProductGateway) {}
+
+    async execute(): Promise<FindAllProductsDto> {
+        const products = await this.productRepository.findAll();
+
+        return {
+            products: products.map((product) => ({
+                id: product.id.id,
+                name: product.name,
+                description: product.description,
+                salesPrice: product.salesPrice,
+            })),
+        };
+    }
+}
