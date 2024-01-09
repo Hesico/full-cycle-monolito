43c1f833cc43bc1d5f4ae62df752760524194296
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2024-01-09 15:44:14 -0300

    feat: InvoiceItem sequelize model

---
+++ src/modules/invoice/repository/invoiceItem.model.ts
@@ -0,0 +1,22 @@
+import { BelongsTo, Column, ForeignKey, Model, PrimaryKey, Table } from "sequelize-typescript";
+import { InvoiceModel } from "./invoice.model";
+
+@Table({ tableName: "invoiceItems", timestamps: false })
+export class InvoiceItemModel extends Model {
+    @PrimaryKey
+    @Column
+    id: string;
+
+    @Column({ allowNull: false })
+    name: string;
+
+    @Column({ allowNull: false })
+    price: number;
+
+    @BelongsTo(() => InvoiceModel)
+    declare product: InvoiceModel;
+
+    @ForeignKey(() => InvoiceModel)
+    @Column({ allowNull: false })
+    declare invoiceId: string;
+}
