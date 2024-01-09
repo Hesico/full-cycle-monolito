b3b9d71a18ee262dcb1f15b6f1d1db2488475c6a
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2024-01-09 15:46:09 -0300

    feat: InvoiceFacadeInterface

--- src/modules/payment/facade/facade.interface.ts
+++ src/modules/payment/facade/facade.interface.ts
@@ -1,11 +1,17 @@
-import UsecaseInterface from "../../@shared/domain/usecase/usecase.interface";
-import PaymentFacadeInterface from "./facade.interface";
-import { PaymentFacadeInputDto, PaymentFacadeOutputDto } from "./payment.facade";
+export interface PaymentFacadeInputDto {
+    orderId: string;
+    amount: number;
+}
 
-export default class PaymentFacade implements PaymentFacadeInterface {
-    constructor(private _processPaymentUseCase: UsecaseInterface) {}
+export interface PaymentFacadeOutputDto {
+    transactionId: string;
+    orderId: string;
+    amount: number;
+    status: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
-    process(input: PaymentFacadeInputDto): Promise<PaymentFacadeOutputDto> {
-        return this._processPaymentUseCase.execute(input);
-    }
+export default interface PaymentFacadeInterface {
+    process(input: PaymentFacadeInputDto): Promise<PaymentFacadeOutputDto>;
 }
