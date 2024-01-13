f030581bae916f0e9ea3935f2e45471a2c3b2c12
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2024-01-13 09:57:03 -0300

    feat: Checkout Controller

---
+++ src/infraestructure/api/controller/checkout.controller.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from "express";
+import UsecaseInterface from "../../../modules/@shared/domain/usecase/usecase.interface";
+
+export default class CheckoutController {
+    private _placeOrderUseCase: UsecaseInterface;
+
+    constructor(placeOrderUseCase: UsecaseInterface) {
+        this._placeOrderUseCase = placeOrderUseCase;
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        const order = req.body;
+
+        try {
+            const output = await this._placeOrderUseCase.execute({
+                clientId: order.clientId,
+                products: order.products,
+            });
+
+            res.status(201).json({
+                status: "sucess",
+                data: {
+                    data: output,
+                },
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
