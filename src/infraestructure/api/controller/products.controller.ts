8522eac88ba3205205dc736fad5a3916bea145e4
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2024-01-13 13:35:07 -0300

    feat: Product Controller

---
+++ src/infraestructure/api/controller/products.controller.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import UsecaseInterface from "../../../modules/@shared/domain/usecase/usecase.interface";
+
+export default class ProductsController {
+    private _addProductUseCase: UsecaseInterface;
+
+    constructor(addProductUseCase: UsecaseInterface) {
+        this._addProductUseCase = addProductUseCase;
+    }
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        const product = req.body;
+
+        try {
+            const output = await this._addProductUseCase.execute({
+                name: product.name,
+                description: product.description,
+                purchasePrice: product.purchasePrice,
+                stock: product.stock,
+            });
+
+            res.status(201).json({
+                status: "sucess",
+                data: {
+                    data: output,
+                },
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
