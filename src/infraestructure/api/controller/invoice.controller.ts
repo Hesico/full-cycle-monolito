76c588a35098337b60753648009ba5e5f64a82d6
Henrique Coutinho <henrique.coutinho@engenharia.ufjf.br>
2024-01-13 09:57:19 -0300

    feat: invoice Controller

---
+++ src/infraestructure/api/controller/invoice.controller.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import UsecaseInterface from "../../../modules/@shared/domain/usecase/usecase.interface";
+
+export default class InvoiceController {
+    private _findInvoiceUseCase: UsecaseInterface;
+
+    constructor(findInvoiceUseCase: UsecaseInterface) {
+        this._findInvoiceUseCase = findInvoiceUseCase;
+    }
+
+    async findOne(req: Request, res: Response, next: NextFunction) {
+        try {
+            const output = await this._findInvoiceUseCase.execute({
+                id: req.params.id,
+            });
+
+            res.status(201).json({
+                status: "sucess",
+                data: {
+                    data: output,
+                },
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
